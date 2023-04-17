29d1e4ef79e7fff77fae528fc4bd11558cfbb0ac
youss95 <usekim7555@gmail.com>
2023-04-17 21:48:38 +0900

    백엔드와 통신 테스트

---
+++ src/api/requestMethods.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import qs from 'qs';
+const BASE_URL = 'http://localhost:8080';
+
+const request = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-type': 'application/json',
+  },
+  params: {},
+  paramsSerializer: params => {
+    return qs.stringify(params, { arrayFormat: 'repeat' });
+  },
+});
+
+request.interceptors.request.use(
+  config => {
+    const token = sessionStorage.getItem('tokenId');
+
+    if (typeof token !== 'string' || !token) return config;
+
+    config.headers.Authorization = `Bearer ${token}`;
+
+    return config;
+  },
+
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+request.interceptors.response.use(
+  response => {
+    return response;
+  },
+
+  error => {
+    if (error) {
+      console.log(error);
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export default request;
--- src/pages/task/MyTask.js
+++ src/pages/task/MyTask.js
@@ -1,6 +1,11 @@
+import request from 'api/requestMethods';
 import Task from 'components/task/my/Task';
 
 const MyTask = () => {
+  const resp = request.get('/api/task/today', {
+    params: { wkId: 1 },
+  });
+  console.log('r', resp);
   return <Task />;
 };
 
